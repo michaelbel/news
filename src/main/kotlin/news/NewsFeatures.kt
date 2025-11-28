16fe78bda68202843fe86894343ddc0d714a17af
michaelbel <michael-bel@outlook.com>
2025-11-27 23:03:43 -0300

    Update project

--- src/main/kotlin/news/NewsFeatures.kt
+++ src/main/kotlin/news/NewsFeatures.kt
@@ -5,5 +5,5 @@ object NewsFeatures {
     const val ANDROID_BLOG_ENABLED: Boolean = false
     const val KOTLIN_BLOG_ENABLED: Boolean = false
     const val ANDROID_WEEKLY_ENABLED: Boolean = false
-    const val ENABLE_PRO_ANDROID_DEV: Boolean = true
+    const val PRO_ANDROID_DEV_ENABLED: Boolean = true
 }
