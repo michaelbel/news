ae11cf8977c5f859725b8bea2e40036a7461433c
michaelbel <michael-bel@outlook.com>
2025-11-29 17:46:00 -0300

    Update project

---
+++ src/main/kotlin/news/firebaseblog/FirebaseBlogItem.kt‎.kt
@@ -0,0 +1,10 @@
+package news.firebaseblog
+
+import news.NewsItem
+import java.time.Instant
+
+data class FirebaseBlogItem(
+    override val published: Instant,
+    override val title: String,
+    override val url: String
+): NewsItem
