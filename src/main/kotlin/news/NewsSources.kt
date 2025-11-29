0e33e10b1484a68355bbfd9030ef1c3287018426
michaelbel <michael-bel@outlook.com>
2025-11-29 13:37:47 -0300

    Update project

--- src/main/kotlin/news/NewsSources.kt
+++ src/main/kotlin/news/NewsSources.kt
@@ -8,25 +8,25 @@ const val PRO_ANDROID_DEV_URL = "https://proandroiddev.com/feed"
 const val ANDROID_WEEKLY_URL = "https://androidweekly.net/rss"
 const val HABR_ANDROID_URL = "https://habr.com/ru/rss/hubs/android_dev/articles/all/?fl=ru"
 val GITHUB_REPOS = listOf(
-    "https://github.com/square/retrofit",
-    "https://github.com/material-components/material-components-android",
-    "https://github.com/InsertKoinIO/koin",
-    "https://github.com/coil-kt/coil",
-    "https://github.com/Kotlin/kotlinx.coroutines",
     "https://github.com/google/dagger",
-    "https://github.com/ReactiveX/RxJava",
-    "https://github.com/airbnb/lottie-android",
     "https://github.com/google/gson",
-    "https://github.com/androidbroadcast/ViewBindingPropertyDelegate",
-    "https://github.com/facebook/facebook-android-sdk",
-    "https://github.com/JakeWharton/timber",
-    "https://github.com/square/leakcanary",
-    "https://github.com/square/okhttp",
+    "https://github.com/material-components/material-components-android",
+    "https://github.com/JetBrains/compose-multiplatform",
+    "https://github.com/Kotlin/kotlinx.coroutines",
     "https://github.com/Kotlin/kotlinx.serialization",
     "https://github.com/ktorio/ktor",
-    "https://github.com/JetBrains/compose-multiplatform",
+    "https://github.com/square/okhttp",
+    "https://github.com/square/retrofit",
+    "https://github.com/square/leakcanary",
+    "https://github.com/ReactiveX/RxJava",
+    "https://github.com/ReactiveX/RxKotlin",
     "https://github.com/ReactiveX/RxAndroid",
-    "https://github.com/ReactiveX/RxKotlin"
+    "https://github.com/InsertKoinIO/koin",
+    "https://github.com/coil-kt/coil",
+    "https://github.com/airbnb/lottie-android",
+    "https://github.com/androidbroadcast/ViewBindingPropertyDelegate",
+    "https://github.com/facebook/facebook-android-sdk",
+    "https://github.com/JakeWharton/timber"
 )
 val YOUTUBE_CHANNELS = listOf(
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCVHFbqXqoYvEWM1Ddxl0QDg", // AndroidDevelopers
@@ -34,7 +34,6 @@ val YOUTUBE_CHANNELS = listOf(
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCKsqMPIIhev3qbMxCL8Emvw", // AndroidBroadcast
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCQFhFs4Ff1BP__DWMD5gC5g", // gulyaev_it
     "https://www.youtube.com/feeds/videos.xml?channel_id=UC71omjio31Esx7LytaZ2ytA", // JovMit
-    "https://www.youtube.com/feeds/videos.xml?channel_id=UCofyDdGnCssPNwABNkxLFKg", // randrushchenko
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCP7uiEZIqci43m22KDl0sNw", // Kotlin
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCIjEgHA1vatSR2K4rfcdNRg", // AristiDevs
     "https://www.youtube.com/feeds/videos.xml?channel_id=UCSmp6mEF9bml3sinNuKcZZg", // offer_factory
