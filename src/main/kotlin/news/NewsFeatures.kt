2f870a1a9e84621a3da8328bfef26637185e7959
michaelbel <michael-bel@outlook.com>
2025-12-01 16:57:24 -0300

    Update project

--- src/main/kotlin/news/NewsFeatures.kt
+++ src/main/kotlin/news/NewsFeatures.kt
@@ -1,14 +1,14 @@
 package news
 
-const val ANDROID_BLOG_ENABLED = false
-const val ANDROID_STUDIO_BLOG_ENABLED = false
+const val ANDROID_BLOG_ENABLED = true
+const val ANDROID_STUDIO_BLOG_ENABLED = true
 const val ANDROID_WEEKLY_ENABLED = true
-const val FIREBASE_BLOG_ENABLED = false
-const val GITHUB_RELEASES_ENABLED = false
-const val GITHUB_TRENDING_KOTLIN_ENABLED = false
-const val HABR_ANDROID_ENABLED = false
-const val KOTLIN_BLOG_ENABLED = false
-const val MEDIUM_ANDROID_ENABLED = false
-const val MEDIUM_GOOGLE_ENABLED = false
-const val PRO_ANDROID_DEV_ENABLED = false
-const val YOUTUBE_ENABLED = false
+const val FIREBASE_BLOG_ENABLED = true
+const val GITHUB_RELEASES_ENABLED = true
+const val GITHUB_TRENDING_KOTLIN_ENABLED = true
+const val HABR_ANDROID_ENABLED = true
+const val KOTLIN_BLOG_ENABLED = true
+const val MEDIUM_ANDROID_ENABLED = true
+const val MEDIUM_GOOGLE_ENABLED = true
+const val PRO_ANDROID_DEV_ENABLED = true
+const val YOUTUBE_ENABLED = true
